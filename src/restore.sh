efc2b3f31c92a7697431e1edbe64354a19204a19
theplatypus <nicolas.bloyet@gmail.com>
2025-02-20 14:05:46 +0100

    [feat] adapt restore.sh

--- src/restore.sh
+++ src/restore.sh
@@ -7,7 +7,7 @@ source ./env.sh
 
 s3_uri_base="s3://${S3_BUCKET}/${S3_PREFIX}"
 
-if [ -z "$PASSPHRASE" ]; then
+if [ -z "${PASSPHRASE:-}" ]; then
   file_type=".dump"
 else
   file_type=".dump.gpg"
@@ -15,30 +15,34 @@ fi
 
 if [ $# -eq 1 ]; then
   timestamp="$1"
-  key_suffix="${POSTGRES_DATABASE}_${timestamp}${file_type}"
+  key_suffix="mongo_${MONGO_DATABASE}_${timestamp}${file_type}"
 else
   echo "Finding latest backup..."
   key_suffix=$(
-    aws $aws_args s3 ls "${s3_uri_base}/${POSTGRES_DATABASE}" \
+    aws $aws_args s3 ls "${s3_uri_base}/" \
       | sort \
       | tail -n 1 \
       | awk '{ print $4 }'
   )
 fi
 
-echo "Fetching backup from S3..."
+echo "Fetching ${key_suffix} from S3..."
 aws $aws_args s3 cp "${s3_uri_base}/${key_suffix}" "db${file_type}"
 
-if [ -n "$PASSPHRASE" ]; then
+if [ -n "${PASSPHRASE:-}" ]; then
   echo "Decrypting backup..."
   gpg --decrypt --batch --passphrase "$PASSPHRASE" db.dump.gpg > db.dump
   rm db.dump.gpg
 fi
 
-conn_opts="-h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DATABASE"
-
 echo "Restoring from backup..."
-pg_restore $conn_opts --clean --if-exists db.dump
+mongorestore --archive=db.dump \
+             --host $MONGO_HOST \
+             --port $MONGO_PORT \
+             --username $MONGO_USER \
+             --password $MONGO_PASSWORD \
+             --authenticationDatabase "admin" \
+             ${MONGORESTORE_EXTRA_OPTS:-}
 rm db.dump
 
 echo "Restore complete."
